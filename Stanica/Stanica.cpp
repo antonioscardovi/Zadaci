801c52e4de91649c6c104dc5fd12ab461b40fb08
Antonio Scardovi <>
2019-03-08 16:10:54 +0100

    Stanica

---
+++ Stanica/Stanica.cpp
@@ -0,0 +1,16 @@
+
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int S, b1, b2, stanica1, stanica2;
+	cin >> S >> b1 >> b2;
+
+	stanica1 = b1 - S;
+	stanica2 = b2 + b1 - S;
+}
+
+
