77d9eb9dbf14a8483c90a0cb8a3d0f4defc38289
Antonio Scardovi <antonio.scardovi@gmail.com>
2019-03-11 22:26:12 +0100

    Smart

---
+++ Smart/Smart.cpp
@@ -0,0 +1,11 @@
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n1, n2, n3, n4, n5, n6;
+	cin >> n1 >> n2 >> n3 >> n4 >> n5 >> n6;
+	cout << n2 << endl << n4 << endl << n6;
+}
