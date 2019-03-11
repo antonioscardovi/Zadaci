3b14565a6beb97f7adc757178aea4cca9cd4ceec
Antonio Scardovi <>
2019-03-11 14:23:35 +0100

    Gjuro

--- Gjuro/Gjuro.cpp
+++ Gjuro/Gjuro.cpp
@@ -6,38 +6,27 @@ using namespace std;
 
 int main()
 {
-	int n;
-	cin >> n;
+	int a, b;
+	cin >> a;
 
-	if (n % 5 == 0) {
-		cout << n;
+	b = a;
+
+	if (a % 5 == 0) {
+		cout << a;
+		return 0;
 	}
-	else if (n % 100 % 10 == 9) {
-		n++;
-	}
-	else if (n % 100 % 10 == 8) {
-		n + 2;
-	}
-	else if (n % 100 % 10 == 7) {
-		n - 2;
-	}
-	else if (n % 100 % 10 == 6) {
-		n - 1;
-	}
-	else if (n % 100 % 10 == 4) {
-		n + 1;
-	}
-	else if (n % 100 % 10 == 3) {
-		n + 2;
-	}
-	else if (n % 100 % 10 == 2) {
-		n - 2;
-	}
-	else if (n  % 10 == 1) {
-		n - 1;
+
+	while (a % 5 != 0 && b % 5 != 0) {
+		a++;
+		b--;
 	}
 	
-	cout << n;
+	if (a % 5 == 0) {
+		cout << a;
+	}
+	else if (b % 5 == 0) {
+		cout << b;
+	}
 }
 
 
