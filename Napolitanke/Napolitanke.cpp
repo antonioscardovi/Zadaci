4af3ff051ab88147cddd174ee262808aaa4e0288
Antonio Scardovi <>
2019-03-12 14:37:26 +0100

    Napolitanke

--- Napolitanke/Napolitanke.cpp
+++ Napolitanke/Napolitanke.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-	int kutija = 40, otv1, kut1, otv2, kut2, p_nap=0, p_kut=1;
+	int kutija = 40, otv1, kut1, otv2, kut2, p_nap=0, p_kut=0;
 	cin >> otv1 >> kut1 >> otv2 >> kut2;
 
 	if (kut1 == kut2) {
@@ -15,10 +15,11 @@ int main()
 	else if (kut2 < kut1) {
 		p_nap = (otv1)+(40 - otv2) + (kutija * (kut1 - kut2 -1));
 		cout << p_nap << endl;
-		do {
-			p_kut++;
-			kutija += kutija;
-		} while (kutija < p_nap);
-		cout << p_kut;
+		p_kut = p_nap / 40;
+		if (p_nap % 40 == 0) {
+			cout << p_kut;
+		}
+		cout << p_kut + 1;
 	}
+	
 }
