80cce3ec841861a3f6fad44ffd548d39d5cc2827
Antonio Scardovi <antonio.scardovi@gmail.com>
2019-03-12 22:03:34 +0100

    Melman

--- Melman/Melman.cpp
+++ Melman/Melman.cpp
@@ -1,25 +1,28 @@
 #include "pch.h"
 #include <iostream>
 using namespace std;
-
 int main()
 {
-	int sat, min, N, t;
-	cin >> sat >> min >> N >> t;
-
+	int sat, min, N, t[10];
+	int maxt = 0;
+	int sat1 = 0, min1 = 0;
+	cin >> sat >> min >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> t[i];
+		min = min + 30;
+		if (min >= 60) {
+			sat++;
+			min = min - 60;
+		}
+		if (t[i] > maxt) {
+			maxt = t[i];
+			sat1 = sat;
+			min1 = min;
+		}
+	}
 	if (N == 0) {
 		cout << t << sat << min;
+		return 0;
 	}
-
-	for (int i = 0; i < N; i++) {
-		cin >> t;
-		min = min + 30;
-	}
-
-	if (min > 60) {
-		sat++;
-	}
-
-
-
+	cout << maxt << endl << sat1 << endl << min1;
 }
