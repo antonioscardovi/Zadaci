919c6a4fa6293278270e369fb4910e1df59bcd87
Antonio Scardovi <>
2019-03-13 09:27:37 +0100

    1 protiv 100

--- 1protiv100/1protiv100.cpp
+++ 1protiv100/1protiv100.cpp
@@ -5,26 +5,29 @@ using namespace std;
 int main()
 {
 	int N, ostali = 100;
-	int dobitak = 1;
+	int iznos = 100000;
+	int dobitak = 0;
 	int podjela;
 	char Mirko;
 
 	do {
-		cin >> Mirko >> N;
+		cin >> Mirko;
 		if (Mirko == 'F') {
+			cin >> N;
 			dobitak = 0;
 			break;
 		}
-		if (N == 100) {
-			dobitak = 100000;
-			break;
+		if (Mirko == 'T') {
+			cin >> N;
+			podjela = iznos / ostali;
+			dobitak += (podjela*N);
+			ostali -= N;
 		}
-		ostali = ostali - N;
-		cout << "Ostali: " << ostali << endl;
-		podjela = 100000 / ostali;
-		cout << "Podjela: " << podjela << endl;
-		dobitak = dobitak + (podjela*N);
-		cout << "Dobitak: " << dobitak << endl;
+		if (Mirko == 'P') {
+			cin >> N;
+			ostali -= N;
+		}
+		
 	} while (ostali > 0);
 
 
