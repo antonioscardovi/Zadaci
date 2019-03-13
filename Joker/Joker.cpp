a6c57ba71597ff09fc76b2c0417714a55bdfb4c0
Antonio Scardovi <>
2019-03-13 12:12:25 +0100

    Joker

--- Joker/Joker.cpp
+++ Joker/Joker.cpp
@@ -6,34 +6,40 @@ using namespace std;
 
 int main()
 {
-	int a, b, c, d, e, f;
-	int serijski1, serijski2, serijski3;
+	int broj, brojevi[6], sifre[3];
 
-	cin >> a >> b >> c >> d >> e >> f;
+	for (int i = 0; i < 6; i++) {
+		cin >> broj;
+		brojevi[i] = broj % 10;
+	}
 
-	a = a % 10;
-	b = b % 10;
-	c = c % 10;
-	d = d % 10;
-	e = e % 10;
-	f = f % 10;
-
-	int rez = stoi(to_string(a) + to_string(b) + to_string(c) + to_string(d) + to_string(e) + to_string(f));
-
-	cout << rez;
-	
-
-	/*cin >> serijski1 >> serijski2 >> serijski3;
-
-	cout << serijski1;*/
+	for (int i = 0; i < 3; i++) {
+		cin >> broj;
+		sifre[i] = broj;
+	}
 
 
-	//if (brojevi[5] % 10 ==  ) {
-	//	cout << "I. vrsta";
-	//}
+	for (int i = 0; i < 3; i++) {
 
-	
+		if (brojevi[5] != sifre[i] % 10 || brojevi[4] != sifre[i] / 10 % 10) {
+			cout << "Nedobitan" << endl;
+		}
+		else if (brojevi[3] != sifre[i] / 100 % 10) {
+			cout << "V. vrsta" << endl;
+		}
+		else if (brojevi[2] != sifre[i] / 1000 % 10) {
+			cout << "IV. vrsta" << endl;
+		}
+		else if (brojevi[1] != sifre[i] / 10000 % 10) {
+			cout << "III. vrsta" << endl;
+		}
+		else if (brojevi[0] != sifre[i] / 100000 % 10) {
+			cout << "II. vrsta" << endl;
+		}
+		else if (brojevi[0] == sifre[i] / 100000 % 10) {
+			cout << "I. vrsta" << endl;
+		}
+	}
 
 
-		
 } 
